6b4a10514e3a6f51cf25b07b3b680dfaa75348d2
Abu Al Maruf <mdmaruf55667711@gmail.com>
2024-06-08 00:39:16 +0600

    added env file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCESKu_ROCD9rHOrnzead84Jr22fMDeSBw",
-  authDomain: "a10-brandshop.firebaseapp.com",
-  projectId: "a10-brandshop",
-  storageBucket: "a10-brandshop.appspot.com",
-  messagingSenderId: "8016749182",
-  appId: "1:8016749182:web:8b1d8fcf67bb8ce9ba6e2f",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
